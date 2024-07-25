1dcb8a52f4e42251dc4079b40cae22a88e4c8709
jongddak <hfsmsj316@gmail.com>
2024-07-25 23:56:10 +0900

    coding test 0725
    coding test 0725

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -1,16 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CodingTest
 {
     internal class Program
     {
+      
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+           
+            Console.WriteLine("시작");
+            //202407025
+            //https://school.programmers.co.kr/learn/courses/30/lessons/120810?language=csharp
+            int solution1(int num1, int num2)
+            {
+                int answer = num1 % num2;
+                return answer;
+            }
+            //https://school.programmers.co.kr/learn/courses/30/lessons/120803
+            int solution2(int num1, int num2)
+            {
+                int answer = num1 - num2;
+                return answer;
+            }
+            //https://school.programmers.co.kr/learn/courses/30/lessons/120807
+            int solution3(int num1, int num2)
+            {
+                int answer = 0;
+                if (num1 == num2) { answer = 1; }
+                else answer = -1;
+
+                return answer;
+            }
+            //https://school.programmers.co.kr/learn/courses/30/lessons/120891
+            public int solution4(int order)
+            {
+                int answer = 0;
+                string x = order.ToString();
+                foreach (char i in x)
+                {
+                    if (i == ('3')) { answer++; }
+                    else if (i == ('6')) { answer++; }
+                    else if (i == ('9')) { answer++; }
+                }
+                return answer;
+            }
+
+
+
+
+
+
+
+
+
         }
     }
 }
+
