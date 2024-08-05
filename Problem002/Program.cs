d9f7215e19d2d8b7b972bc7a0517ce98dde00e35
jongddak <hfsmsj316@gmail.com>
2024-08-05 17:14:51 +0900

    x만큼 간격이 있는 n개의 숫자
    x만큼 간격이 있는 n개의 숫자

--- Problem002/Program.cs
+++ Problem002/Program.cs
@@ -3,9 +3,31 @@
     internal class Program
     {
         //https://school.programmers.co.kr/learn/courses/30/lessons/181936
-        public static void Solution1() 
+
+        public int solution1(int number, int n, int m)
         {
-            
+            int answer = 0;
+            if (number % n == 0)
+            {
+                if (number % m == 0)
+                {
+                    answer = 1;
+                }
+            }
+
+            return answer;
+        }
+
+
+        //https://school.programmers.co.kr/learn/courses/30/lessons/12954
+        public long[] solution2(int x, int n)
+        {
+            long[] answer = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                answer[i] += (long)x * (i + 1);
+            }   // 그냥 int로 냅두면 int 범위를 벗어나서 오류뜸
+            return answer;
         }
 
 
