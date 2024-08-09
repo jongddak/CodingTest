a6c1291518e5d7856e4713f9979483a9a93d8c46
jongddak <hfsmsj316@gmail.com>
2024-08-09 11:59:28 +0900

    MiddleNum
    MiddleNum

--- MiddleNumber/Program.cs
+++ MiddleNumber/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace MiddleNumber
+﻿using System;
+
+namespace MiddleNumber
+
 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120811?language=csharp
     internal class Program
     {
+        public int solution(int[] array)
+        {
+            int answer = 0;
+            Array.Sort(array);
+            answer = array[array.Length/2];
+
+
+            return answer;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
